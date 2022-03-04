a397ca4545c2e08324f43cb3ad23af79b32c4c98
bakosandi <bakso89@gmail.com>
2022-03-04 08:59:53 +0100

    add components

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Flying Circus</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit,
+        repudiandae.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut libero
+        autem fugiat quo voluptates maxime ipsa adipisci neque fugit minus iure
+        minima dolores perferendis dicta, sequi blanditiis et quas repellat
+        atque quasi temporibus perspiciatis, facere consequatur enim! Quaerat
+        illum nam numquam debitis a nobis dolorem tempore blanditiis, harum
+        voluptate placeat?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
